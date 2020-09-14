c67ca1d49351cb4a0b14b3613cfab6c9d4ce2ced
EddieOrozco <orozcoe230@gmail.com>
2020-09-13 23:31:05 -0500

    Created Product Component added it to Home comp/passing props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './Product';
 import './Home.css';
 
 function Home() {
@@ -8,6 +9,13 @@ function Home() {
         className='home_image'
         src='https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg'
         alt=''
+      />
+      <Product
+        id='1'
+        title='The Lean Startup: How Constant Innovates'
+        price={10.25}
+        rating={5}
+        image='//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fsites.psu.edu%2Fsiowfa15%2Fwp-content%2Fuploads%2Fsites%2F29639%2F2015%2F11%2F3827791_01_10.jpg&f=1&nofb=1'
       />
     </div>
   );
