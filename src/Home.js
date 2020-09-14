a9fc0c48532347a808549fe77741530e9393aad7
EddieOrozco <orozcoe230@gmail.com>
2020-09-13 22:28:35 -0500

    Added Home page banner

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      <h1>I am the home page</h1>
+    <div className='home'>
+      <img
+        className='home_image'
+        src='https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg'
+        alt=''
+      />
     </div>
   );
 }
