eb50326d98ab89f31083f19cd881f52631592ed4
EddieOrozco <orozcoe230@gmail.com>
2020-09-29 20:53:20 -0500

    Finish Checkout Page/Component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 import './Checkout.css';
 
 function Checkout() {
   const [{ basket }] = useStateValue();
   return (
     <div className='checkout'>
-      <img
-        className='cheackout_add'
-        src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
-        alt=''
-      />
-      {basket?.length === 0 ? (
-        <div>
-          <h2>Your Shopping Basket is Empty</h2>
-        </div>
-      ) : (
-        <div>
-          <h2>Your Shopping Basket </h2>
+      <div className='checkout_left'>
+        <img
+          className='cheackout_add'
+          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+          alt=''
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is Empty</h2>
+            <p>
+              You have no items in your basket. To buy one or more items, click
+              "Add to basket" next to the item.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 className='checkout_title'>Your Shopping Basket </h2>
+
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className='checkout_right'>
+          <h1>Subtotal</h1>
         </div>
       )}
     </div>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,22 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: '5',
+      title: 'Forget the sun stay fresh and cool.',
+      price: 105.99,
+      rating: 5,
+      image:
+        '//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.framesdirect.com%2Fproduct_elarge_images%2Frayban-sun-rb8301-002-56-angle.jpg&f=1&nofb=1',
+    },
+    {
+      id: '5',
+      title: 'Forget the sun stay fresh and cool.',
+      price: 105.99,
+      rating: 5,
+      image:
+        '//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.framesdirect.com%2Fproduct_elarge_images%2Frayban-sun-rb8301-002-56-angle.jpg&f=1&nofb=1',
+    },
+  ],
 };
 
 function reducer(state, action) {
@@ -12,7 +29,21 @@ function reducer(state, action) {
       break;
     case 'REMOVE_FROM_BASKET':
       // Logic for Removing item to basket
-      return { state };
+
+      // Cloned the basket
+      let newBasket = [...state.basket];
+      // Check to see if product exists
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Cant remove product (id:${action.id})`);
+      }
+
+      return { ...state, basket: newBasket };
       break;
     default:
       return state;
